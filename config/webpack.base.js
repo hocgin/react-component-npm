a73bb93cde7e3212eba4e6d4354fdba1cfe65824
hocgin <hocgin@gmail.com>
2020-10-22 23:09:47 +0800

    update

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -29,7 +29,7 @@ module.exports = {
         loader: "less-loader"
       }],
     }, {
-      test: /\.(png|svg|jpg|gif)$/,
+      test: /\.(png|svg|jpg|gif|ttf)$/,
       use: {
         loader: 'url-loader'
       }
